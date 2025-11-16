a8565df873bc46bec8865f577c54bda996491e20
RTOmega <62565267+RebornTrack970@users.noreply.github.com>
2025-11-16 12:50:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    printf("Would you like to use a file or enter your own expression?\n");
+    printf("1: file\n2: enter in console\n");
     
+    return 0;
+
 }
