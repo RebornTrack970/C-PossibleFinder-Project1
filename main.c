20697e6a9aeb030abc2f10d05f9d06134119a8db
RTOmega <62565267+RebornTrack970@users.noreply.github.com>
2025-11-16 12:54:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 int main(void) {
     printf("Would you like to use a file or enter your own expression?\n");
     printf("1: file\n2: enter in console\n");
-    
+    int num;
+    scanf("%d",&num);
+    char arr[30];
+    if (num==1) {
+        fgets(arr, sizeof(arr), stdin);
+    }else if (num==2) {
+
+    }else {
+        printf("Unknown Choice Spesified. Exiting Program.");
+        exit(0);
+    }
     return 0;
 
 }
